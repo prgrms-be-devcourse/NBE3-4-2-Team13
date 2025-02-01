d29a61e97667a6b46899d26d1b7cda7a4043632f
swjoon <jetkid1446@gmail.com>
2025-02-01 15:40:12 +0900

    fix : filesevice 내 권한 부여 코드 삭제

--- backend/src/main/java/com/app/backend/domain/attachment/service/LocalStorageFileServiceImpl.java
+++ backend/src/main/java/com/app/backend/domain/attachment/service/LocalStorageFileServiceImpl.java
@@ -35,7 +35,6 @@ public class LocalStorageFileServiceImpl implements FileService {
             // 디렉토리가 존재하지 않으면 생성
             if (!Files.exists(dateDir)) {
                 Files.createDirectories(dateDir);
-                Files.setPosixFilePermissions(dateDir, PosixFilePermissions.fromString("rwxrwxrwx"));
             }
 
             String originalFileName = file.getOriginalFilename();
