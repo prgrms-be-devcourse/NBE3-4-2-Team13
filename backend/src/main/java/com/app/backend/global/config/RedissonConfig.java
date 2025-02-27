33282908b645cf84f89882da58ad17d0c03f9bdb
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-02-27 10:24:33 +0900

    fix: Redisson 전용 Redis 서버 설정 추가
    - Redis 서버 분리 및 설정 값 추가

--- backend/src/main/java/com/app/backend/global/config/RedissonConfig.java
+++ backend/src/main/java/com/app/backend/global/config/RedissonConfig.java
@@ -3,16 +3,23 @@ package com.app.backend.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${redisson.host}")
+    private String host;
+
+    @Value("${redisson.port}")
+    private int port;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379").setConnectionPoolSize(64);
+        config.useSingleServer().setAddress("redis://%s:%d".formatted(host, port)).setConnectionPoolSize(64);
         return Redisson.create(config);
     }
 
