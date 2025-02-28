8e57aa21af1a73cfb62fef007545e2f540374812
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-02-28 11:13:16 +0900

    fix: import 누락 수정

--- backend/src/main/java/com/app/backend/domain/group/service/GroupMembershipService.java
+++ backend/src/main/java/com/app/backend/domain/group/service/GroupMembershipService.java
@@ -13,6 +13,7 @@ import com.app.backend.domain.group.repository.GroupMembershipRepository;
 import com.app.backend.domain.group.repository.GroupRepository;
 import com.app.backend.domain.notification.dto.NotificationEvent;
 import com.app.backend.domain.notification.service.NotificationService;
+import com.app.backend.global.annotation.CustomLock;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
