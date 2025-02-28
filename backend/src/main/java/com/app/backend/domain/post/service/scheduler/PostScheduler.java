de88ae74532e7c5ba20f8167c27574e03025c3b7
swjoon <jetkid1446@gmail.com>
2025-02-28 15:39:25 +0900

    feat : scheduler 로 soft delete 된 게시물과 파일들 삭제 - 현재 삭제 기준 1주일

--- backend/src/main/java/com/app/backend/domain/post/service/scheduler/PostScheduler.java
+++ backend/src/main/java/com/app/backend/domain/post/service/scheduler/PostScheduler.java
@@ -1,7 +1,11 @@
 package com.app.backend.domain.post.service.scheduler;
 
+import com.app.backend.domain.attachment.service.FileService;
 import com.app.backend.domain.post.entity.Post;
+import com.app.backend.domain.post.entity.PostAttachment;
 import com.app.backend.domain.post.repository.post.PostRepository;
+import com.app.backend.domain.post.repository.postAttachment.PostAttachmentRepository;
+import com.app.backend.global.config.FileConfig;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +13,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -18,10 +25,16 @@ import java.util.Set;
 public class PostScheduler {
 
     private final RedisTemplate<String, Object> redisTemplate;
+
+    private final FileConfig fileConfig;
+    private final FileService fileService;
     private final PostRepository postRepository;
+    private final PostAttachmentRepository postAttachmentRepository;
+
     private static final String POST_UPDATE = "post:update";
     private static final String POST_HISTORY = "post:history";
     private static final String VIEW_COUNT_PREFIX = "viewCount:post:postid:";
+    private static final int deleteDays = 7;
 
     @Transactional
     @Scheduled(fixedRate = 600_000) // 10분
@@ -35,7 +48,16 @@ public class PostScheduler {
         processViewCountSave(POST_HISTORY, true);
     }
 
-    private void processViewCountSave(String typeKey, boolean isReset) {
+    @Transactional
+    @Scheduled(cron = "0 0 4 * * ?")
+    public void deletePosts() {
+        LocalDateTime deleteDay = LocalDate.now().minusDays(deleteDays).atStartOfDay();
+
+        processDeletePosts(deleteDay);
+        processDeleteFiles(deleteDay);
+    }
+
+    private void processViewCountSave(final String typeKey, final boolean isReset) {
         try {
             Set<Object> updatedPostIds = redisTemplate.opsForSet().members(typeKey);
 
@@ -74,5 +96,28 @@ public class PostScheduler {
         } catch (Exception e) {
             log.error("데이터 동기화에 실패했습니다");
         }
+    }
+
+    private void processDeletePosts(final LocalDateTime deleteDay) {
+        postRepository.deleteAllByModifiedAtAndDisabled(deleteDay, true);
+    }
+
+    private void processDeleteFiles(final LocalDateTime deleteDay) {
+        List<PostAttachment> files = postAttachmentRepository.findAllByModifiedAtAndDisabled(deleteDay, true);
+
+        if(files.isEmpty()){
+            return;
+        }
+
+        List<String> deleteFilePaths = new ArrayList<>();
+        List<Long> deleteFileIds = new ArrayList<>();
+
+        files.forEach(file -> {
+            deleteFileIds.add(file.getId());
+            deleteFilePaths.add("%s/%s".formatted(fileConfig.getBASE_DIR(), file.getStoreFilePath()));
+        });
+
+        postAttachmentRepository.deleteByFileIdList(deleteFileIds);
+        fileService.deleteFiles(deleteFilePaths);
     }
 }
